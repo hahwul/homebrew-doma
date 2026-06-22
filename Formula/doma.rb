8c7f7914f32f3d3693b55845752e95bfb2d20e65
hahwul <hahwul@gmail.com>
2026-06-22 15:59:42 +0000

    doma 0.4.0

--- Formula/doma.rb
+++ Formula/doma.rb
@@ -6,7 +6,7 @@
 class Doma < Formula
   desc "Directory tag manager - put your directories on the cutting board"
   homepage "https://github.com/hahwul/doma"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   # doma ships prebuilt binaries: Linux is statically linked (musl) and macOS
@@ -15,28 +15,28 @@ class Doma < Formula
   # guard in release-binary.yml enforces that no Homebrew dylib leaks in.
   on_macos do
     on_arm do
-      url "https://github.com/hahwul/doma/releases/download/v0.3.0/doma-v0.3.0-osx-arm64"
-      sha256 "df0e0817d0eca4984e62ddec2cbcbedbd12da323a5e6915401e9bfb7afb14ca2"
+      url "https://github.com/hahwul/doma/releases/download/v0.4.0/doma-v0.4.0-osx-arm64"
+      sha256 "d5fa53b953e6aa7a1dfe47cc659c0f952371a0503292acdde233b2bcbcb893f7"
     end
     on_intel do
-      url "https://github.com/hahwul/doma/releases/download/v0.3.0/doma-v0.3.0-osx-x86_64"
-      sha256 "2839d53c3f5852c88b0aa7a5459efb58ba4e9bb49071a0da82f4f917c26d4355"
+      url "https://github.com/hahwul/doma/releases/download/v0.4.0/doma-v0.4.0-osx-x86_64"
+      sha256 "1fd0a4ec0f8274ae91898b4462b4b413725ec6bee322010c9676c37a86706909"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/hahwul/doma/releases/download/v0.3.0/doma-v0.3.0-linux-arm64"
-      sha256 "b1cd1a36395bf7117c89061dd51cdf689a66ae7a5ba938798268b1816c720a79"
+      url "https://github.com/hahwul/doma/releases/download/v0.4.0/doma-v0.4.0-linux-arm64"
+      sha256 "334df6647b9a24d02541b6846c248c97ee016f3ec9444a8bce713f8e76505e32"
     end
     on_intel do
-      url "https://github.com/hahwul/doma/releases/download/v0.3.0/doma-v0.3.0-linux-x86_64"
-      sha256 "4944ac849cf5f12d3178c002d2f373a157baefa6f08bee4529cf8a0a8ebda592"
+      url "https://github.com/hahwul/doma/releases/download/v0.4.0/doma-v0.4.0-linux-x86_64"
+      sha256 "aaefba0880fa721a652a8bcce8b9fe28f9d807bf8b63f7eda341856c26ce6882"
     end
   end
 
   def install
-    bin.install Dir["doma-v0.3.0-*"].first => "doma"
+    bin.install Dir["doma-v0.4.0-*"].first => "doma"
   end
 
   test do
